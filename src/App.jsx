893e6079a5b270d20590420a25ca191313b33b0b
SamuelMarinTobon <smarinto@correo.iue.edu.co>
2024-10-07 18:08:14 -0500

    arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-import Home from './components/Home/Home';
 import Ingreso from './components/Ingreso/Ingreso';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Ingreso />} />
-        <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<Ingreso />} />
+  
       </Routes>
     </Router>
   );
--- src/components/Ingreso/Ingreso.jsx
+++ src/components/Ingreso/Ingreso.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import './Ingreso.css';
 import logo from '../../assets/logo.png';
+import './Ingreso.css';
 
 export default function Ingreso() {
   return (
-      <div className='principal'>
-        <div className='ContainerIngreso'>
-          <img src={logo} alt='Logo' className='logo' />
-          <h1 className='EstebanquitoIngreso'>Iniciar sesion</h1>
-          <input type='text' className='InputIngreso' placeholder='Usuario' />
-          <input type='text' className='InputIngreso' placeholder='Contraseña' />
-          <button className='BotonIngresara'>Ingresar</button>
-          <p>
-            ¿No tienes Cuenta?
-            <a href=''>Registro</a>
-          </p>
-        </div>
+    <div className='principal'>
+      <div className='ContainerIngreso'>
+        <img src={logo} alt='Logo' className='logo' />
+        <h1 className='EstebanquitoIngreso'>Iniciar sesion</h1>
+        <input type='text' className='InputIngreso' placeholder='Usuario' />
+        <input type='text' className='InputIngreso' placeholder='Contraseña' />
+        <button className='BotonIngresara'>Ingresar</button>
+        <p>
+          ¿No tienes Cuenta?
+          <a href=''>Registro</a>
+        </p>
       </div>
-  
+    </div>
   );
 }
