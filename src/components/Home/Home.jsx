915a2607f3eafd1ede43b26b63acf6cc5ff11cda
Ruben <rlondono@alico-sa.com>
2024-10-07 18:05:02 -0500

    Agrego home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import './Home.css';
 
 export default function Home() {
@@ -10,3 +10,44 @@ export default function Home() {
     </div>
   );
 }
+*/
+
+import React from 'react';
+import './Home.css'; 
+import Logo from '../../assets/logo4.png'; 
+import Fondo from '../../assets/fondo4.png'; 
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <header className="home-header">
+        <div className='home-logo'>
+          <img src={Logo} alt="SwiftM Logo" className="home-logo" />
+        </div>
+        <div className="home-buttons">
+            <button className="btn-create-account">Crear Cuenta</button>
+            <button className="btn-login">Iniciar Sesión</button>
+        </div>
+      </header>
+      
+      <div className="home-content">
+        <div className="home-text">
+          <h1>Seguridad y Confianza</h1>
+          <p>
+            Disfruta de tranquilidad con nuestra sitio web bancario seguro y confiable. 
+            Tu bienestar financiero es nuestra prioridad.
+          </p>
+          <div className="home-buttons">
+            <button className="btn-about-us">Conoce más</button>
+          </div> 
+        </div>
+        
+        <div className="home-fondo">
+          <img src={Fondo} alt="Fondo de Seguridad" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
