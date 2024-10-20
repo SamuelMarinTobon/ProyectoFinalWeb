1464b2d75b1493b60bc09521b68d4e3d81ef86f4
michelserna31 <masab0731@gmail.com>
2024-10-20 13:16:24 -0500

    Retiros actualizado

--- src/components/Retiro/Retiro.jsx
+++ src/components/Retiro/Retiro.jsx
@@ -58,14 +58,3 @@ export default function InterfazRetiros() {
     </div>
   );
 }
-import React from 'react';
-
-const Retiro = () => {
-  return (
-    <div>
-      <h1>retiro</h1>
-    </div>
-  );
-};
-
-export default Retiro;
